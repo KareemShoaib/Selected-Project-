dff5e0a829957adb86cb04a40bf1bc4cd59cd370
Mazen Tayseer <maziiin360@gmail.com>
2025-06-07 06:42:28 +0300

    added test.csv

--- scripts/extract_small_portion.py
+++ scripts/extract_small_portion.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 ROWS_TO_EXTRACT = 5000
 
-df = pd.read_csv('csv/clean/train.csv')
+df = pd.read_csv('csv/clean/test.csv')
 df = df.head(ROWS_TO_EXTRACT)
-df.to_csv('csv/small/train.csv', index=False)
-print(f"Extracted {ROWS_TO_EXTRACT} rows from train.csv")
+df.to_csv('csv/small/test.csv', index=False)
+print(f"Extracted {ROWS_TO_EXTRACT} rows from test.csv")
--- scripts/fix_entities.py
+++ scripts/fix_entities.py
@@ -1,6 +1,6 @@
 import html
 
-with open('csv/train.csv', 'r', encoding='utf-8') as f:
+with open('csv/raw/test.csv', 'r', encoding='utf-8') as f:
     content = f.read()
 
 content = content.replace('&amp;', '&')
@@ -10,7 +10,7 @@ content = content.replace('&lt;', '<')
 content = content.replace('&gt;', '>')
 
 content = html.unescape(content)
-with open('csv/clean/train.csv', 'w', encoding='utf-8') as f:
+with open('csv/clean/test.csv', 'w', encoding='utf-8') as f:
     f.write(content)
 
 print('All HTML entities have been decoded!') 
